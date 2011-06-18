8580570bdee3a61b5205d6bbfdfa49201e4ce920
ianwookim <ianwookim@gmail.com>
2011-06-18 08:56:16 +0000

    add Acidic addJob and queryFirstJob

--- src/HEP/Automation/JobQueue/JobQueue.hs
+++ src/HEP/Automation/JobQueue/JobQueue.hs
@@ -21,16 +21,25 @@
 module HEP.Automation.JobQueue.JobQueue where
 
 import Control.Applicative
+import Control.Monad.State
+import Control.Monad.Reader
+import Data.Typeable
+
+import Data.Acid
+
 
 import HEP.Automation.JobQueue.JobType
 
+import Prelude hiding (length)
+import Data.Sequence
 import Data.SafeCopy
+
 
 data JobInfo = JobInfo {
   jobinfo_id     :: Int, 
   jobinfo_detail :: JobDetail, 
   jobinfo_status :: JobStatus
-}
+} deriving Typeable
 
 data JobDetail = EventGen EventSet | MathAnal EventSet 
 
@@ -56,3 +65,19 @@ instance SafeCopy JobStatus where
 
 $(deriveSafeCopy 0 'base ''JobDetail)
 $(deriveSafeCopy 0 'base ''JobInfo)
+
+newtype JobInfoQueue = JobInfoQueue (Seq JobInfo)
+
+$(deriveSafeCopy 0 'base ''JobInfoQueue)
+
+addJob :: JobInfo -> Update JobInfoQueue ()
+addJob job = do JobInfoQueue jq <- get 
+                put $ JobInfoQueue (jq |> job) 
+
+queryFirstJob :: Query JobInfoQueue (Maybe JobInfo)
+queryFirstJob = do JobInfoQueue jq <- ask 
+                   if length jq == 0 
+                     then return Nothing
+                     else return (Just (index jq 0))
+
+$(makeAcidic ''JobInfoQueue ['addJob, 'queryFirstJob])
