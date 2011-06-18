e5fca5cafe1878dd4fb7b829d17ed75398db9847
ianwookim <ianwookim@gmail.com>
2011-06-18 13:09:11 +0000

    testjob tested

--- src/HEP/Automation/JobQueue/Client/Command.hs
+++ src/HEP/Automation/JobQueue/Client/Command.hs
@@ -6,10 +6,27 @@ import Network.HTTP.Types
 import Network.HTTP.Enumerator
 
 
+import HEP.Automation.MadGraph.Model 
+import HEP.Automation.MadGraph.Machine
+import HEP.Automation.MadGraph.UserCut
+import HEP.Automation.MadGraph.SetupType
+
+
+import HEP.Automation.JobQueue.JobType
+import HEP.Automation.JobQueue.JobJson
+
+testjob_psetup = PS MadGraph4 DummyModel "test" "test" "test" 
+testjob_rsetup = RS DummyParam 10000 LHC7 Fixed 200.0 NoMatch NoCut NoPYTHIA NoUserCutDef NoPGS 1
+
+testjob = EventSet testjob_psetup testjob_rsetup 
+
 jobqueueTest = do 
   putStrLn "test"
   manager <- newManager
   requestget <- parseUrl ("http://127.0.0.1:3600/")
+  let testjson = toAeson testjob
+ 
+  print testjson
   r <- httpLbs requestget manager
   putStrLn $ show r
 
