75dfd8d9a8e7f3f01e6cd0983efd7d867fe0f9a6
ianwookim <ianwookim@gmail.com>
2011-06-19 00:29:29 +0000

    first successful communication with jobqueueserver with POST method and aeson type.

--- src/HEP/Automation/JobQueue/Client/Command.hs
+++ src/HEP/Automation/JobQueue/Client/Command.hs
@@ -4,7 +4,9 @@ module HEP.Automation.JobQueue.Client.Command where
 
 import Network.HTTP.Types
 import Network.HTTP.Enumerator
-
+import qualified Data.ByteString.Lazy as L 
+import qualified Data.ByteString.Lazy.Char8 as C
+import qualified Data.ByteString.Char8 as SC
 
 import HEP.Automation.MadGraph.Model 
 import HEP.Automation.MadGraph.Machine
@@ -14,6 +16,8 @@ import HEP.Automation.MadGraph.SetupType
 
 import HEP.Automation.JobQueue.JobType
 import HEP.Automation.JobQueue.JobJson
+
+import Data.Aeson.Encode
 
 testjob_psetup = PS MadGraph4 DummyModel "test" "test" "test" 
 testjob_rsetup = RS DummyParam 10000 LHC7 Fixed 200.0 NoMatch NoCut NoPYTHIA NoUserCutDef NoPGS 1
@@ -31,10 +35,27 @@ jobqueueTest = do
   putStrLn $ show r
 
 
+jobqueueTest2 :: String -> IO () 
+jobqueueTest2 str = do 
+  putStrLn "test2"
+  manager <- newManager 
+  request <- parseUrl ("http://127.0.0.1:3600/queue")
+  
+  let testjson = encode $ toAeson testjob 
+
+  let myrequestbody = RequestBodyLBS testjson -- (C.pack testjson) 
+  
+  let requestpost = request { method = methodPost, 
+                              requestHeaders = [ ("Content-Type", "text/plain") ], 
+                              requestBody = myrequestbody } 
+  r <- httpLbs requestpost manager 
+  putStrLn $ show r 
+
 
 commandLineProcess :: [String] -> IO () 
 commandLineProcess args = do 
   print args 
   case args !! 0 of
     "test" -> jobqueueTest 
+    "test2" -> jobqueueTest2 "oohhhoooh"
 
