cad98955fa7ecf6cb752e2ddaa4a4495cf11d8e5
ianwookim <ianwookim@gmail.com>
2011-07-07 06:02:17 +0000

    add server_main for ServerConfig

--- src/HEP/Automation/JobQueue/Server/Type.hs
+++ src/HEP/Automation/JobQueue/Server/Type.hs
@@ -3,5 +3,6 @@ module HEP.Automation.JobQueue.Server.Type where
 import HEP.Storage.WebDAV.Type
 
 data ServerConfig = ServerConfig { 
+  server_main   :: String, 
   server_webdav :: WebDAVServer 
 } deriving (Show)
