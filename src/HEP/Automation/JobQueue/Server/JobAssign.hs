18ae62ccc772dea1d0e82c942c46aa3de5c908d3
ianwookim <ianwookim@gmail.com>
2011-09-10 04:21:17 +0000

    add Missing JobAssign.hs

---
+++ src/HEP/Automation/JobQueue/Server/JobAssign.hs
@@ -0,0 +1,29 @@
+module HEP.Automation.JobQueue.Server.JobAssign where
+
+import qualified Data.IntMap as IM
+
+import HEP.Automation.JobQueue.Config
+import HEP.Automation.JobQueue.JobQueue
+
+import Data.List 
+import Data.Maybe
+
+checkJobCompatibility :: ClientConfiguration -> JobInfo -> Bool 
+checkJobCompatibility (ClientConfiguration _cname math _pbs montecarlo _dsdir) 
+                      jobinfo =
+  case jobinfo_detail jobinfo of 
+    EventGen _ _ -> montecarlo
+    MathAnal _ _ _ -> math  
+
+findFirstJob :: ClientConfiguration -> ([JobInfo],[JobInfo]) -> Maybe JobInfo
+findFirstJob cc (unassigned,finished) = do 
+  let compatible = filter (checkJobCompatibility cc) unassigned
+      finished_assoc = map (\x->(jobinfo_id x,x)) finished
+      checkDependency jinfo = do 
+        let deps = jobinfo_dependency jinfo
+        mapM_ (\x->lookup x finished_assoc) deps
+        return jinfo
+      workable = mapMaybe checkDependency compatible
+  if null workable
+    then Nothing 
+    else Just (head compatible)
