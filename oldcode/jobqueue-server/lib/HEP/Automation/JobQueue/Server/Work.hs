eff5430a0e6b22a969ab68382ee229ade09ee030
Ian-Woo Kim <ianwookim@gmail.com>
2014-08-04 12:33:57 +0200

    remove configparser dependency

--- oldcode/jobqueue-server/lib/HEP/Automation/JobQueue/Server/Work.hs
+++ oldcode/jobqueue-server/lib/HEP/Automation/JobQueue/Server/Work.hs
@@ -1,10 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module HEP.Automation.JobQueue.Server.Work where
 
 import Text.Parsec 
 import Control.Monad.Identity
+import Control.Monad.Trans.Maybe
 import Control.Exception (bracket)
-
-import HEP.Parser.Config
+import Data.Configurator as C
+import Data.Configurator.Types 
+-- 
+-- import HEP.Parser.Config
 import HEP.Storage.WebDAV.Type 
 import HEP.Automation.JobQueue.Server.Type
 
@@ -12,6 +17,16 @@ import System.IO
 
 serverConfigParser :: FilePath -> IO ServerConfig
 serverConfigParser fp = do 
+  config <- load [Required fp]
+  r <- runMaybeT $ do
+    url <- MaybeT (C.lookup config "server.mainURL")
+    webdavurl <- MaybeT (C.lookup config "server.webdavURL")
+    return (ServerConfig url (GlobalURL webdavurl))
+  case r of
+    Nothing -> error "serverConfigParser configuration failed"
+    Just c -> return c
+
+{-
   putStrLn ("parsing server config file " ++ fp )
   bracket (openFile fp ReadMode) hClose $ \fh -> do 
     str <- hGetContents fh -- readFile fp
@@ -19,7 +34,7 @@ serverConfigParser fp = do
     r `seq` case r of 
               Left msg -> error (show msg)
               Right sconf -> return $! sconf 
-  
+ 
 
 configServer :: ParsecT String () Identity ServerConfig
 configServer = do 
@@ -27,3 +42,4 @@ configServer = do
     url <- oneFieldInput "mainURL"
     webdavurl <- oneFieldInput "webdavURL"
     return (ServerConfig url (GlobalURL webdavurl))
+-}
